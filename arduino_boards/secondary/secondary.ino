a69fa3a13898f118c7edd914ce8ddf3515299c94
samdai01 <daisam1215@gmail.com>
2022-03-14 10:11:52 -0700

    secondary board pin setup

---
+++ arduino_boards/secondary/secondary.ino
@@ -0,0 +1,12 @@
+/**
+ * Code for the secondary Arduino Uno board that stores and plays the last three songs.
+ * March 1, 2022
+ */
+
+// buzzer pin
+#define buzzer 10
+ 
+// communication pins with the other boards
+#define com0 2 
+#define com1 3
+#define com2 4
